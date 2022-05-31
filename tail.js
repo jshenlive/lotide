44ca9855ce11750e3df8fa2447356f8d9551e8a0
Jesse Shen <jly.shen@gmail.com>
2022-05-31 05:18:26 +0000

    added tail.js

---
+++ tail.js
@@ -0,0 +1,22 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const tail = function(arr) {
+  let newArr = arr;
+  return newArr.slice(1);
+};
+
+// TEST CASE
+let test = [5,6,7,8];
+let testResult = tail(test);
+console.log(test);
+console.log(testResult);
+
+assertEqual(test.length, 4);
+

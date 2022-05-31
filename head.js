973bacaf74068f19898b9fbdebf017cc1d70ef44
Jesse Shen <jly.shen@gmail.com>
2022-05-31 05:10:36 +0000

    added head.js

--- head.js
+++ head.js
@@ -1,4 +1,3 @@
-// FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
@@ -7,8 +6,9 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs",'Bootcamp');
-assertEqual(1,1);
-assertEqual("helloWorld","helloWorld");
-assertEqual(123,321);
+const head = function(arr) {
+  return arr[0];
+};
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello","Lighthouse","Labs"]), "Hello");

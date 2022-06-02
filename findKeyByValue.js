6cf57752e25b5d52e729956575c9b3d5a5a49128
Jesse Shen <jly.shen@gmail.com>
2022-06-02 03:48:45 +0000

    added findKeyByValue.js

---
+++ findKeyByValue.js
@@ -0,0 +1,26 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function (data, value) {
+  for (let key in data) {
+    if (data[key].includes(value)) {
+      return key;
+    }
+  }
+  return undefined;
+};
+
+// const bestTVShowsByGenre = {
+//   sci_fi: "The Expanse",
+//   comedy: "Brooklyn Nine-Nine",
+//   drama: ["House of Cards", "The Wire", "Suits"],
+// };
+
+// assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+// assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+// assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");

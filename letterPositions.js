1051dbf3772b172160d8e65ebc6d24599ad1bff5
Jesse Shen <jly.shen@gmail.com>
2022-06-02 03:14:05 +0000

    added letterPositions.js

---
+++ letterPositions.js
@@ -0,0 +1,38 @@
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function (arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🔴🔴🔴Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+const letterPositions = function (sentence) {
+  const results = {};
+  // logic to update results here
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] !== " ") {
+      if (!results[sentence[i]]) {
+        results[sentence[i]] = [];
+        results[sentence[i]].push(i);
+      } else {
+        results[sentence[i]].push(i);
+      }
+    }
+  }
+
+  return results;
+};
+// assertArraysEqual(letterPositions("hello").e, [1]);
+// assertArraysEqual(letterPositions("hello").l, [2, 3]);
